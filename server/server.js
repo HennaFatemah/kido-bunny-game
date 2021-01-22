843e7ff5caed2acf95750e42619a6528422794ab
Henna Fatemah <henna_fatemah@yahoo.com>
2021-01-22 14:29:34 -0500

    app listening on port

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 const express = require ('express');
 const app = express();
 const cors = require ('cors');
-const port = process.env.PORT || 2021
+
+//..dotenv
+require ('dotenv').config();
+const port = process.env.PORT || 2021;
+
+//....allow cors
+app.use(cors());
+
+app.listen(port, () => console.log(`listening on port ${port}`));
