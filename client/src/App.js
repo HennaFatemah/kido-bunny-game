e80ad9d1706b3a078b5a5beb26d8510a08834d6e
Henna Fatemah <henna_fatemah@yahoo.com>
2021-01-23 11:12:34 -0500

    added background and tower and zoom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+// import Background from './assets/mainImages/background-image.jpg'
+import GateLeft from './assets/mainImages/lookout-tower-gate-1.png'
+import GateRight from './assets/mainImages/lookout-tower-gate-2.png'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    scale: 1,
+  }
+  
+  zoomIn = () => {
+     this.setState({ scale: this.state.scale * 4 });
+  }
+  zoomOut = () => {
+    this.setState({ scale: this.state.scale / 4 });
+  }
+  render() {
+    return (
+      <div className="container">
+        {/* <img className="container__background" src={Background} alt="Background"/> */}
+        <div
+          className="container__bg-image-wrapper"
+          style={{ 'transform': `scale(${this.state.scale})` }}
+        />
+        <div className="container__left-gate-ctnr">
+          <img className="container__left-gate" src={GateLeft} alt="left gate"/>
+        </div>
+        <div className="container__right-gate-ctnr">
+          <img className="container__right-gate" src={GateRight} alt="right gate"/>
+        </div>
+        <div className="container__content">
+          <button className="container__click" onClick={this.zoomIn}>Click on the <br/>LOOKOUT TOWER</button>
+          <button onClick={this.zoomOut}>Exit</button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
