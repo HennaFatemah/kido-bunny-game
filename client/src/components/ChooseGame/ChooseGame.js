3dcc2ada5cad9a81f46fe46161c8ad24fb66ea4a
Henna Fatemah <henna_fatemah@yahoo.com>
2021-02-13 13:46:27 -0500

    choose game page tested on mobile and tablet portrait views

--- client/src/components/ChooseGame/ChooseGame.js
+++ client/src/components/ChooseGame/ChooseGame.js
@@ -11,7 +11,7 @@ const ChooseGame = () => {
                     <h2 className="choose__secondary-head">Choose Your Game</h2>
                     <div className="choose__instruction-container">
                         <h2 className="choose__instruction">Instructions</h2>
-                        <h3 className="choose__pointers"><span><img className="choose__paw-img"src={Paw} alt='paw pointer'/></span>Click on your favorite pup to play the game</h3>
+                        <h3 className="choose__pointers"><span><img className="choose__paw-img"src={Paw} alt='paw pointer'/></span>Click on your favorite bunny to play the game</h3>
                         <h3 className="choose__pointers"><span><img className="choose__paw-img" src={Paw} alt='paw pointer'/></span>Click on the arrows to change the slide</h3>
                     </div>
                 </div>
--- client/src/components/ChooseGame/Slides/SlideOne/SlideOne.js
+++ client/src/components/ChooseGame/Slides/SlideOne/SlideOne.js
@@ -1,7 +1,7 @@
 
 import {Link} from 'react-router-dom';
 import Skye from '../../../../assets/pups/Color-bunny-1.png';
-import Zuma from '../../../../assets/pups/zuma-paw-patrol-png-41900.png';
+import Zuma from '../../../../assets/pups/bunny-flippy.png';
 import RiderOne from '../../../../assets/rider/rider-angle-1.png';
 import RiderTwo from '../../../../assets/rider/rider-angle-2.png';
 import RiderMain from '../../../../assets/rider/rider-paw-patrol-png-41895.png'
@@ -29,14 +29,14 @@ class SlideOne extends Component {
                 {this.state.riderPosition === 1 ? <img className="slide-one__rider-up" src={RiderOne} alt="Rider with hand up"/> : this.state.riderPosition === 2 ? <img className="slide-one__rider-down" src={RiderTwo} alt="Rider with hand down"/> : this.state.riderPosition === 3 ? <img className="slide-one__rider-main" src={RiderMain} alt="Rider dancing"/> : <img className="slide-one__rider-main" src={RiderMain} alt="Rider dancing"/>}
                 <Link className="slide-one__link" to='/choosegame/addgame'>
                     <div className="slide-one__tile" onMouseOver={LinkTo} onMouseLeave={LinkLeave}>
-                        <h3 className="slide-one__tile-title">Add with Bunny Berry</h3>
-                        <img className="slide-one__tile-pic" src={Skye} alt="Pup named Skye"/>
+                        <h3 className="slide-one__tile-title slide-one__tile-title--berry">Add with Bunny Berry</h3>
+                        <img className="slide-one__tile-pic slide-one__tile-pic--berry" src={Skye} alt="Pup named Skye"/>
                     </div>
                 </Link>
                 <Link className="slide-one__link" to='/choosegame/subtractgame'>
                     <div className="slide-one__tile" onMouseOver={LinkDown} onMouseLeave={LinkLeave}>
-                        <h3 className="slide-one__tile-title">Subtract with Zuma</h3>
-                        <img className="slide-one__tile-pic" src={Zuma} alt="Pup named Zuma"/>
+                        <h3 className="slide-one__tile-title">Subtract with Bunny Flippy</h3>
+                        <img className="slide-one__tile-pic slide-one__tile-pic--flippy" src={Zuma} alt="Pup named Zuma"/>
                     </div>
                 </Link>
             </div>
