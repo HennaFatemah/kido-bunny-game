ed5e095a9d73fe25c5bd1fc2654c19e2a7de060d
Henna Fatemah <henna_fatemah@yahoo.com>
2021-02-13 17:27:34 -0500

    slide two tested on mobile and tablet portrait view

--- client/src/components/ChooseGame/Slides/SlideTwo/SlideTwo.js
+++ client/src/components/ChooseGame/Slides/SlideTwo/SlideTwo.js
@@ -1,7 +1,7 @@
 
 import {Link} from 'react-router-dom';
-import Marshal from '../../../../assets/pups/new-marshal-paw-patrol-png.png';
-import Everest from '../../../../assets/pups/everest-paw-patrol-png-41896.png';
+import Marshal from '../../../../assets/pups/bunny-toughy.png';
+import Everest from '../../../../assets/pups/bunny-venger.png';
 import RiderOne from '../../../../assets/rider/rider-angle-1.png';
 import RiderTwo from '../../../../assets/rider/rider-angle-2.png';
 import RiderMain from '../../../../assets/rider/rider-paw-patrol-png-41895.png'
@@ -28,15 +28,15 @@ class SlideTwo extends Component {
             <div className="slide-two">
                 {this.state.riderPosition === 1 ? <img className="slide-two__rider-up" src={RiderOne} alt="Rider with hand up"/> : this.state.riderPosition === 2 ? <img className="slide-two__rider-down" src={RiderTwo} alt="Rider with hand down"/> : this.state.riderPosition === 3 ? <img className="slide-two__rider-main" src={RiderMain} alt="Rider dancing"/> : <img className="slide-two__rider-main" src={RiderMain} alt="Rider dancing"/>}
                 <div className="slide-two__tile" onMouseOver={LinkTo} onMouseLeave={LinkLeave}>
-                    <h3 className="slide-two__tile-title">Exercise with Marshal</h3>
+                    <h3 className="slide-two__tile-title slide-two__tile-title--toughy">Exercise with Bunny Toughy</h3>
                     <Link className="slide-two__link" to='/choosegame/exercisegame'>
-                        <img className="slide-two__tile-pic" src={Marshal} alt="Pup named Marshal"/>
+                        <img className="slide-two__tile-pic slide-two__tile-pic--toughy" src={Marshal} alt="Pup named Marshal"/>
                     </Link>
                 </div>
                 <div className="slide-two__tile" onMouseOver={LinkDown} onMouseLeave={LinkLeave}>
-                    <h3 className="slide-two__tile-title">Scavenger hunt with Everest</h3>
+                    <h3 className="slide-two__tile-title">Scavenger hunt with Bunny Venger</h3>
                     <Link className="slide-two__link" to='/choosegame/scavengerhunt'>
-                        <img className="slide-two__tile-pic" src={Everest} alt="Pup named Everest"/>
+                        <img className="slide-two__tile-pic slide-two__tile-pic--venger" src={Everest} alt="Pup named Everest"/>
                     </Link>
                 </div>
             </div>
